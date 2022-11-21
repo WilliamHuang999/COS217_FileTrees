8fa8a26b0b01c6f5f79bf61812a5a3a33d3bc5ae
George Tziampazis <george.tzi@princeton.edu>
2022-11-20 20:11:31 -0500

    ft.c

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -24,7 +24,7 @@
 /* 1. a flag for being in an initialized state (TRUE) or not (FALSE) */
 static boolean bIsInitialized;
 /* 2. a pointer to the root node in the hierarchy */
-static Node_T oNRoot;
+static NodeD_T oNRoot;
 /* 3. a counter of the number of nodes in the hierarchy */
 static size_t ulCount;
 
